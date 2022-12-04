898a54f063de9db2eb58ef825c0d8c6034354eba
Huynh Cong Hung <huynhconghung131@gmail.com>
2022-12-04 22:33:49 +0700

    Add project files.

---
+++ CinemaBookingSystem.ClientWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CinemaBookingSystem.ClientWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
